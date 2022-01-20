5251141e35eb56814ef51a4c5a340ff82268ce77
soroush <soroushjorjorzade@gmail.com>
2022-01-20 17:42:07 +0330

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ferox on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
